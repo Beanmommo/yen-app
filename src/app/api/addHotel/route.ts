d43d59925536af0687295a2aa94dc34b51ae03fc
Beanmommo <leonardo.prasetyo5@gmail.com>
2024-04-25 00:05:01 +1000

    chore: changed initial data for add hotel

--- src/app/api/addHotel/route.ts
+++ src/app/api/addHotel/route.ts
@@ -33,6 +33,7 @@ export async function POST(req: Request) {
   }
 
   return NextResponse.json({
+    id: hotelData.id,
     message: "hotel created very nice",
   });
 }
--- src/lib/action.ts
+++ src/lib/action.ts
@@ -16,13 +16,13 @@ export async function addHotel(hotelFormData: AddHotelFormData) {
     id: "",
     hotel_name: hotelFormData.hotel_name,
     hotel_address: hotelFormData.hotel_address,
-    hotel_about: "",
+    hotel_about: null,
     cover_image_url: null,
     hotel_images_url: null,
   };
 
   try {
-    await fetch("/api/addHotel", {
+    const res = await fetch("/api/addHotel", {
       method: "POST",
       body: JSON.stringify(data),
     });
