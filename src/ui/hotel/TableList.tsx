5a466397479d1ceafdec1b1f7d7108679ced5eb7
Beanmommo <leonardo.prasetyo5@gmail.com>
2024-05-03 10:22:25 +1000

    chore: introduced renderCell for custom cells

--- src/ui/hotel/TableList.tsx
+++ src/ui/hotel/TableList.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useHotel } from "@/lib/action";
+import { Hotel } from "@/lib/definitions";
 import { dataToTableData, dictArrToArr } from "@/lib/utils";
 import {
   Table,
@@ -10,11 +11,15 @@ import {
   TableRow,
   getKeyValue,
 } from "@nextui-org/table";
+import { useCallback } from "react";
 
 export default function TableList() {
   const [hotelArr, error, isLoading] = useHotel();
-  const hotelData = dictArrToArr(hotelArr);
-  const rows = dataToTableData(hotelData, ["hotel_address", "hotel_name"]);
+  const rows = dataToTableData(dictArrToArr(hotelArr), [
+    "hotel_address",
+    "hotel_name",
+    "id",
+  ]);
   const columns = [
     {
       key: "hotel_name",
@@ -24,7 +29,24 @@ export default function TableList() {
       key: "hotel_address",
       label: "HOTEL ADDRESS",
     },
+    { key: "id", label: "ACTION" },
   ];
+
+  const renderCell = useCallback(
+    (hotel: { [x: string]: any }, columnKey: any) => {
+      const cellValue = hotel[columnKey];
+      switch (columnKey) {
+        case "hotel_name":
+        case "hotel_address":
+          return <p>{cellValue}</p>;
+        case "id": //Action
+          return <p className=" bg-red-500">{cellValue}</p>;
+        default:
+          return <p>{cellValue}</p>;
+      }
+    },
+    [],
+  );
 
   return (
     <Table aria-label="Hotel List Table">
@@ -37,7 +59,7 @@ export default function TableList() {
         {(item) => (
           <TableRow key={item.key}>
             {(columnKey) => (
-              <TableCell>{getKeyValue(item, columnKey)}</TableCell>
+              <TableCell>{renderCell(item, columnKey)}</TableCell>
             )}
           </TableRow>
         )}
